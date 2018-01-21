51877c2bc6295b15fa0e7c7dd2cd0248176929a4
Gabriela <gbizotto@gmail.com>
2018-01-21 19:53:29 -0200

    Added Butterknife and Canarinho (values formatter); reading input data

--- app/src/main/java/br/gbizotto/parkingsaver/budget/view/BudgetActivity.java
+++ app/src/main/java/br/gbizotto/parkingsaver/budget/view/BudgetActivity.java
@@ -1,15 +1,38 @@
 package br.gbizotto.parkingsaver.budget.view;
 
+import android.databinding.DataBindingUtil;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.concretesolutions.canarinho.watcher.ValorMonetarioWatcher;
 import br.gbizotto.parkingsaver.R;
+import br.gbizotto.parkingsaver.budget.viewModel.BudgetViewModel;
+import br.gbizotto.parkingsaver.databinding.ActivityBudgetBinding;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BudgetActivity extends AppCompatActivity {
+
+    @BindView(R.id.edt_budget)
+    TextInputEditText mBudgetEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_budget);
+
+        ActivityBudgetBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_budget);
+        binding.setBudgetViewModel(new BudgetViewModel());
+
+        ButterKnife.bind(this);
+
+        addListeners();
+    }
+
+    private void addListeners() {
+        mBudgetEdt.addTextChangedListener(new ValorMonetarioWatcher.Builder()
+                .comSimboloReal()
+                .comMantemZerosAoLimpar()
+                .build());
     }
 }
